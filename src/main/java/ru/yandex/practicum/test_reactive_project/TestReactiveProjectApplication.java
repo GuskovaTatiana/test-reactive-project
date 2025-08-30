4df8f8615662fca3baafbc416e12724395d9e1ef
terokhovets <terokhovets@mdaudit.ru>
2025-08-30 07:39:21 +0300

    sprint6. v1. The basis of the project

---
+++ src/main/java/ru/yandex/practicum/test_reactive_project/TestReactiveProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.test_reactive_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestReactiveProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestReactiveProjectApplication.class, args);
+	}
+
+}
